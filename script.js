d881ececc3021284a3f4afa0b660e4257c79af90
Hans Fredric Garay <hgaray@sviisca.com>
2022-11-24 14:22:07 +0800

    fully working

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+boardDetails();
+
 const X_CLASS = 'x';
 const O_CLASS = 'o';
 const WINNING_COMBINATIONS = [
@@ -17,25 +19,56 @@ const create = document.getElementById('createGame');
 const winningMessageElement = document.getElementById("winningMessage");
 const restartButton = document.getElementById('restartButton');
 const createButton = document.getElementById('createButton');
+const createInput = document.getElementById('createInput');
 const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+const waiting = document.getElementById('waiting');
+const readyButton = document.getElementById('ready');
+const manualRestartButton = document.getElementById('manualRestart');
 
 let xCount = 0;
 let oCount = 0;
 let boardCheck = [];
 let circleTurn;
 let gameKey = undefined;
+let gameStatus;
 
 restartButton.addEventListener('click', resetGame);
 createButton.addEventListener('click', createGame);
+createInput.addEventListener('keypress', function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        createButton.click();
+    }
+});
+
+readyButton.addEventListener('click', () => {
+    fetch("http://3.210.183.75:8080/tictactoe/tictactoeserver/check?key=" + gameKey)
+        .then(res => { return res.text() })
+        .then(resData => { if (resData != "false") { startGameX() } })
+        .catch(error => console.warn(error));
+});
+
+manualRestartButton.addEventListener('click', () => {
+    fetch("http://3.210.183.75:8080/tictactoe/tictactoeserver/reset?key=" + gameKey)
+        .then(res => res.text())
+        .then(resData => {
+            gameKey = undefined;
+            board.style.display = "none";
+            create.style.display = "flex";
+            waiting.style.display = "none";
+            return resData;
+        })
+})
 
 function createGame() {
-    gameKey = document.getElementById('gameKey').value
+    gameKey = document.getElementById('createInput').value
     fetch("http://3.210.183.75:8080/tictactoe/tictactoeserver/createGame?key=" + gameKey)
         .then(res => res.text())
         .then(resData => {
             if (resData == 'X') {
                 console.log("waiting for other player")
-                checkLoop();
+                create.style.display = "none";
+                waiting.style.display = "flex";
             } if (resData == 'O') {
                 console.log("Let the games begin!")
                 startGameO();
@@ -50,50 +83,52 @@ function resetGame() {
     fetch("http://3.210.183.75:8080/tictactoe/tictactoeserver/reset?key=" + gameKey)
         .then(res => res.text())
         .then(resData => {
-            if (resData == "[EXIT]") {
-                gameKey = undefined;
-                board.style.display = "none";
-                create.style.display = "block";
-                winningMessageElement.classList.remove('show')
-            }
+            gameKey = undefined;
+            board.style.display = "none";
+            create.style.display = "flex";  
+            winningMessageElement.classList.remove('show');
+            return resData;
         })
-    }
-    
-    function startGameX() {
-        create.style.display = "none";
-        setBoardHoverClassX();
-        board.style.display = "grid";
-        circleTurn = false;
-        cellElements.forEach(cell => {
+}
+
+function startGameX() {
+    waiting.style.display = "none";
+    board.style.display = "grid";
+    cellElements.forEach(cell => {
         cell.classList.remove(X_CLASS)
         cell.classList.remove(O_CLASS)
         cell.addEventListener('click', handleClickX)
         cell.addEventListener('click', handleClickX, { once: true })
-        xCount = 0;
-        oCount = 0;
-        })
-    }
+    })
+    circleTurn = false;
+    board.classList.add(X_CLASS);
+    xCount = 0;
+    oCount = 0;
+}
 
-    function startGameO() {
-        create.style.display = "none";
-        setBoardHoverClassO();
-        board.style.display = "grid";
-        circleTurn = false;
-        cellElements.forEach(cell => {
+function startGameO() {
+    create.style.display = "none";
+    board.style.display = "grid";
+    cellElements.forEach(cell => {
         cell.classList.remove(X_CLASS)
         cell.classList.remove(O_CLASS)
         cell.addEventListener('click', handleClickO)
         cell.addEventListener('click', handleClickO, { once: true })
-        xCount = 0;
-        oCount = 0;
-        })
-    }
+    })
+    circleTurn = false;
+    board.classList.add(O_CLASS);
+    xCount = 0;
+    oCount = 0;
+}
 
 function checkLoop() {
     fetch("http://3.210.183.75:8080/tictactoe/tictactoeserver/check?key=" + gameKey)
-        .then(res => {return res.text()})
-        .then(resData => {if(resData == false){
-            checkLoop()}})
+        .then(res => { return res.text() })
+        .then(resData => {
+            if (resData == false) {
+                checkLoop()
+            }
+        })
         .then(startGameX())
         .catch(error => console.warn(error));
 }
@@ -101,111 +136,93 @@ function checkLoop() {
 function check() {
     fetch("http://3.210.183.75:8080/tictactoe/tictactoeserver/check?key=" + gameKey)
         .then(res => res.text())
-        .then(resData => {return resData})
+        .then(resData => { gameStatus = resData; return gameStatus; })
         .catch(error => console.warn(error));
 }
 
 function handleClickX(e) {
     boardDetails();
-    boardCheckCount();
-    if(xCount <= oCount){
+    check();
+    if (xCount <= oCount) {
         const cell = e.target;
         const coordinates = e.target.id.split(",")
         const y = coordinates[0];
         const x = coordinates[1];
-    
+
         fetch("http://3.210.183.75:8080/tictactoe/tictactoeserver/move?key=" + gameKey + "&tile=" + X_CLASS + "&y=" + y + "&x=" + x)
-            .then(res => {return res.text()})
+            .then(res => { return res.text() })
             .then(resData => {
-                console.log(resData)
-                if (resData != "[TAKEN]") {
+                if (resData != "[TAKEN]" && xCount <= oCount) {
                     placeMark(cell, X_CLASS)
                     if (checkWin(X_CLASS)) {
-                        endGame(false)
+                        endGame(false, X_CLASS)
                         console.log("X is the winner!")
                     } else if (isDraw()) {
                         endGame(true)
                     } else {
                         boardDetails();
-                        // swapTurns();
-                        // setBoardHoverClass();
                     }
                 } else {
-                    let i = 0;
-                    do {
-                        boardDetails();
-                        console.log("boardCheck array: " + boardCheck);
-                        if (boardCheck[i] != "") {
-                            placeMark(cell, boardCheck[i])
-                        }
-                        i++
-                    } while (i <= boardCheck.length);
+                    if(!cell.classList.contains(X_CLASS)){
+                        placeMark(cell, O_CLASS)
+                    }
                 }
             })
             .catch(error => console.warn(error));
-    } else {console.log("It's not yet your turn!")}
+    } else {
+        boardDetails();
+        console.log("It's not yet your turn!");
+    }
+    console.log(gameStatus)
+    if (gameStatus == "false") {
+        winningMessageElement.classList.add('show');
+        winningMessageTextElement.innerText = `Game Over!`;
+    }
 }
 
 function handleClickO(e) {
     boardDetails();
-    boardCheckCount();
-    if(xCount > oCount){
-
-        // console.log("clicked");
-        // placeMark
-        // Check for Win
-        // Check for Draw
-        // Switch Turns
-    
+    check();
+    if (xCount > oCount) {
         const cell = e.target;
-        // const currentClass = circleTurn ? O_CLASS : X_CLASS;
-    
+
         const coordinates = e.target.id.split(",")
         const y = coordinates[0];
         const x = coordinates[1];
-    
-        // console.log(y);
-        // console.log(x);
-    
-    
+
         fetch("http://3.210.183.75:8080/tictactoe/tictactoeserver/move?key=" + gameKey + "&tile=" + O_CLASS + "&y=" + y + "&x=" + x)
-            .then(res => {return res.text()})
+            .then(res => { return res.text() })
             .then(resData => {
-                console.log(resData)
-                if (resData != "[TAKEN]") {
+                if (resData != "[TAKEN]" && xCount > oCount) {
                     placeMark(cell, O_CLASS)
                     if (checkWin(O_CLASS)) {
-                        endGame(false)
+                        endGame(false, O_CLASS)
                         console.log("O is the winner!")
+                        
                     } else if (isDraw()) {
                         endGame(true)
                     } else {
                         boardDetails();
-                        boardCheckCount();
-                        // swapTurns();
-                        // setBoardHoverClass();
                     }
                 } else {
-                    let i = 0;
-                    do {                    
-                        boardDetails();
-                        console.log("boardCheck array: " + boardCheck);
-                        if (boardCheck[i] != "") {
-                            placeMark(cell, boardCheck[i])
-                        }
-                        i++
-                    } while (i <= boardCheck.length);
+                    placeMark(cell, X_CLASS)
                 }
             })
             .catch(error => console.warn(error));
-    } else{console.log("It's not yet your turn!")}
+    } else { console.log("It's not yet your turn!") }
+    boardDetails();
+    console.log(gameStatus)
+    if (gameStatus == "false") {
+        winningMessageElement.classList.add('show');
+        winningMessageTextElement.innerText = `Game Over!`;
+    }
 }
 
-function endGame(draw) {
+function endGame(draw, currentClass) {
     if (draw) {
         winningMessageTextElement.innerText = "Draw!"
     } else {
-        winningMessageTextElement.innerText = `${circleTurn ? "O" : "X"} Wins!`
+        winningMessageTextElement.innerText = `${currentClass} Wins!`
     }
     winningMessageElement.classList.add('show')
 }
@@ -220,28 +237,6 @@ function placeMark(cell, currentClass) {
     cell.classList.add(currentClass)
 }
 
-// function swapTurns() {
-//     circleTurn = !circleTurn;
-// }
-
-// function setBoardHoverClass() {
-//     board.classList.remove(X_CLASS)
-//     board.classList.remove(O_CLASS)
-//     if (circleTurn) {
-//         board.classList.add(O_CLASS)
-//     } else {
-//         board.classList.add(X_CLASS)
-//     }
-// }
-
-function setBoardHoverClassO(){
-    board.classList.add(O_CLASS)
-}
-
-function setBoardHoverClassX() {
-    board.classList.add(X_CLASS)
-}
-
 function checkWin(currentClass) {
     return WINNING_COMBINATIONS.some(combination => {
         return combination.every(index => {
@@ -250,22 +245,25 @@ function checkWin(currentClass) {
     })
 }
 
-function boardDetails() {
-    fetch("http://3.210.183.75:8080/tictactoe/tictactoeserver/board?key=" + gameKey)
-        .then(res => {return res.text()})
-        .then(resData => boardCheck = resData.split(":"))
-        .catch(error => console.warn(error));
-}
+async function boardDetails() {
+    await fetch("http://3.210.183.75:8080/tictactoe/tictactoeserver/board?key=" + gameKey)
+        .then(res => { return res.text() })
+        .then(resData => {
+            xCount = 0;
+            oCount = 0;
+            boardCheck = resData.split(":");
+            let i = 0;
+            do {
+                if (boardCheck?.[i] == 'x') {
+                    cellElements[i].classList.add('x')
+                    xCount++;
+                } else if (boardCheck?.[i] == 'o') {
+                    cellElements[i].classList.add('o')
+                    oCount++;
+                } i++
+            } while (i < boardCheck.length);
+        })
+        .then (() => {boardDetails();})
 
-function boardCheckCount(){
-    let i = 0;
-    console.log(boardCheck)
-    xCount = 0;
-    do{
-        if (boardCheck[i] == 'x'){
-            xCount = xCount + 1
-        } else if(boardCheck[i] == 'o'){
-            oCount = oCount + 1
-        } i++
-    } while(i < boardCheck.length);
+        .catch(error => console.warn(error));
 }
